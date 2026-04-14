e7a1cc2ebe823edbfe81f980b371d75240cd1453
Jake Lunn <jake.lunn@changeagents.info>
2026-04-13 20:46:05 -0500

    fix: fixed auto versioning again

--- src/onepace_assistant/__init__.py
+++ src/onepace_assistant/__init__.py
@@ -1,5 +1,3 @@
 """One Pace Assistant - Download and organize One Pace files."""
 
-from importlib.metadata import version
-
-__version__ = version("onepace-assistant")
+from onepace_assistant._version import __version__
